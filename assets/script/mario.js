1f10a1ffed0be90224f01d9aa639306c8bf68b9a
Guilherme Santana <guilherme09augustoadams@gmail.com>
2022-10-25 21:50:28 -0400

    adicionei um reset para recomeçar o jogo

--- assets/script/mario.js
+++ assets/script/mario.js
@@ -1,5 +1,8 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const nuvens = document.querySelector('.nuvens');
+
+const containerGamenOver = document.querySelector('.container-game-over')
 
 const pula = () => {
    mario.classList.add('pula');
@@ -11,18 +14,22 @@ const pula = () => {
 const gameOver = setInterval(() => {
    const pipePosicao = pipe.offsetLeft;
    const marioPosicao = +window.getComputedStyle(mario).bottom.replace('px', '');
-   verifGame(pipePosicao, marioPosicao);
+   const nuvensPosicao = nuvens.offsetLeft;
+   verificaGame(pipePosicao, marioPosicao, nuvensPosicao);
 
 }, 10);
 
 document.addEventListener('keydown', pula);
 
-function verifGame(pipePosicao, marioPosicao) {
+function verificaGame(pipePosicao, marioPosicao, nuvensPosicao) {
 
    if (pipePosicao <= 105 && pipePosicao > 0 && marioPosicao < 90) {
 
       pipe.style.animation = 'none';
       pipe.style.left = `${pipePosicao}px`;
+
+      nuvens.style.animation = 'none';
+      nuvens.style.left = `${nuvensPosicao}px`;
 
       mario.style.animation = 'none';
       mario.style.bottom = `${marioPosicao}px`;
@@ -30,6 +37,14 @@ function verifGame(pipePosicao, marioPosicao) {
       mario.src = 'assets/img/game_over.png'
       mario.style.marginBottom = '-14px';
 
+      containerGamenOver.style.opacity = 1;
+
       clearInterval(gameOver);
    }
 }
+
+const bottomReload = document.querySelector('.container-game-over button');
+
+bottomReload.addEventListener('click', () => {
+   location.reload();
+});
